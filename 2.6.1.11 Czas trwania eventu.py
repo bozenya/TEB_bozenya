f9c2c39fccbf6a38649aec0b015e46b8a8e6384a
bozenya <142731198+bozenya@users.noreply.github.com>
2023-10-28 19:24:34 +0200

    Add files via upload

--- 2.6.1.11 Czas trwania eventu.py
+++ 2.6.1.11 Czas trwania eventu.py
@@ -24,24 +24,24 @@ y = (m+d)  #ilosc minut ktore dodaje uzytkownik
 
 if y > 60:          #gdy minuty eventu przekroczą 60 (min zegarowych)
 
-    k = int(y/60)   #tworzę zmienną k co mi doda godziny
+    k = int(y/60)   #tworzy zmienną k co mi doda godziny
 
-    h += k          #powiększam format godziny o zmienną k
+    h += k          #powiększa format godziny o zmienną k
 
-    m = y - (k*60)  #
+    m = y - (k*60)  #minuty startowe ustawia przez k przeobrażoną na minuty
 
-if h >= 24:
+if h >= 24:         #gdy (dodawany już) czas jest większy od 24
 
-    zmienna = (24 * int((d/60)/24))
+    zmienna = (24 * int((d/60)/24)) #tworzy zmienną, która sprawdza czy czas wydarzenia nie jest 0 i wylicza godzinę do odjęcia (??? nie wiem co tu zaszło)
 
-    if zmienna == 0:
+    if zmienna == 0:                #gdy zmienna to 0
 
-        zmienna = int(d/60)
+        zmienna = int(d/60)         #???
 
-        h -=24
+        h -=24                      #odejmuje z formatu godzin (np. 25,30) liczbę 24 i nadaje własciwy format h
 
     else:
+        h -= zmienna                #odejmuje godziny stworzone przez zmienną = (24 * int((d/60)/24))
 
-        h -= zmienna
+print(h,":", m)                     #przedstawia czas zakończenia np. 20:45
 
-print(h,":", m)
